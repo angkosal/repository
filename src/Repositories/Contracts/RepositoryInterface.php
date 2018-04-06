88b8aa36565e1456bf1bb93709415c47cd8c7137
Ang Kosal <angkosal@gmail.com>
2018-04-06 17:45:16 +0700

    update

--- src/Repositories/Contracts/RepositoryInterface.php
+++ src/Repositories/Contracts/RepositoryInterface.php
@@ -12,7 +12,7 @@ interface RepositoryInterface
 
     public function findWhereFirst($column, $value);
 
-    public function findWhereLike($column, $value);
+    public function findWhereLike($columns, $value);
 
     public function paginate($perPage = 10);
 
